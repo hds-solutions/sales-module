de672e4581152d00035df310b993acbffba9bc6d
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-29 18:11:34 -0400

    Moved POS to separated module

--- routes/sales.php
+++ routes/sales.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use HDSSolutions\Finpar\Http\Controllers\
-    {OrderController, PriceChangeController, TdvController};
+use HDSSolutions\Finpar\Http\Controllers\{
+    OrderController,
+    PriceChangeController,
+};
 
 Route::group([
     'prefix'        => config('backend.prefix'),
@@ -11,17 +13,13 @@ Route::group([
     // name prefix
     $name_prefix = [ 'as' => 'backend' ];
 
-     Route::resource('orders',    OrderController::class,   $name_prefix)
-         ->parameters([ 'orders' => 'resource' ])
-         ->name('index', 'backend.orders');
+    Route::resource('orders',   OrderController::class, $name_prefix)
+        ->parameters([ 'orders' => 'resource' ])
+        ->name('index', 'backend.orders');
 
-    Route::post('orders/price',                       [ PriceChangeController::class, 'price' ])
+    Route::post('orders/price',                 [ PriceChangeController::class, 'price' ])
         ->name('backend.orders.price');
-    Route::post('orders/{resource}/process',          [ OrderController::class, 'processIt'])
+    Route::post('orders/{resource}/process',    [ OrderController::class, 'processIt'])
         ->name('backend.orders.process');
 
-    Route::resource('tdv', TdvController::class, $name_prefix)
-        ->only(["create", 'store'])
-        ->parameters(['tdv' => 'resource'])
-        ->name('create', 'backend.orders.create');
 });
