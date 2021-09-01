8daf505a9021fcc0b507aa288f9a7b7a90842e35
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-08-31 21:05:08 -0400

    Valid scope and attributes to get max and next document_number

--- app/Models/Stamping.php
+++ app/Models/Stamping.php
@@ -3,11 +3,25 @@
 namespace HDSSolutions\Laravel\Models;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Builder;
 
 class Stamping extends X_Stamping {
 
     public function invoices() {
         return $this->hasMany(Invoice::class);
+    }
+
+    public function scopeValid(Builder $query) {
+        // return valid stampings
+        return self::where('valid_from', '<', now())->where('valid_until', '>=', now());
+    }
+
+    public function getMaxDocumentNumberAttribute() {
+        return $this->invoices->max('document_number');
+    }
+
+    public function getNextDocumentNumberAttribute() {
+        return str_increment($this->max_document_number ?? null);
     }
 
 }
