e61089b399d0688a6ca9884145b6c8ea44a4751b
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-11-02 14:49:21 -0300

    Added Stamping.getNextDocumentNumber() method

--- app/Models/Stamping.php
+++ app/Models/Stamping.php
@@ -16,12 +16,19 @@ class Stamping extends X_Stamping {
         return self::where('valid_from', '<', now())->where('valid_until', '>=', now());
     }
 
-    public function getMaxDocumentNumberAttribute():string {
+    public function getMaxDocumentNumberAttribute():?string {
         return $this->invoices->max('document_number');
     }
 
-    public function getNextDocumentNumberAttribute():string {
+    public function getNextDocumentNumberAttribute():?string {
         return str_increment($this->max_document_number ?? null);
+    }
+
+    public function getNextDocumentNumber(string $prepend = '001-001-', bool $with_prepend = true):?string {
+        // get next document number
+        $next = str_increment($this->invoices()->where('document_number', 'like', "$prepend%")->max('document_number'));
+        // return document number
+        return $with_prepend ? $next : str_replace($prepend, '', $next);
     }
 
 }
