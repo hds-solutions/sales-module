2c75072c6858db09f979952d65b13bb38649ccfa
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-11-02 14:51:28 -0300

    FIX: Dispatch event through static method

--- resources/assets/js/resources/Order.js
+++ resources/assets/js/resources/Order.js
@@ -20,7 +20,7 @@ export default class Order extends Document {
             // foreach lines and fire change
             this.lines.forEach(line =>
                 // fire change on first <select> (product selector)
-                this.fire('change', line.container.querySelector('select:first-child'))
+                Order.fire('change', line.container.querySelector('select:first-child'))
             )
         );
     }
